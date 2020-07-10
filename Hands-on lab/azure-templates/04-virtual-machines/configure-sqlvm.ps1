967b90dc608010eae4c492916d4c83727aa71c29
Hiroya Yamamoto <hiroyay@microsoft.com>
2020-07-10 20:05:01 +0900

    データベース アタッチのスクリプトを追加

--- Hands-on lab/azure-templates/04-virtual-machines/configure-sqlvm.ps1
+++ Hands-on lab/azure-templates/04-virtual-machines/configure-sqlvm.ps1
@@ -10,3 +10,26 @@ function Disable-InternetExplorerESC {
 
 # Disable IE ESC
 Disable-InternetExplorerESC
+
+Invoke-WebRequest 'https://raw.githubusercontent.com/hiroyay-ms/Server-Migration-Hands-on-Lab/master/Hands-on%20lab/src/ContosoInsurance.zip' -OutFile 'C:\ContosoInsurance.zip'
+Expand-Archive -LiteralPath 'C:\ContosoInsurance.zip' -DestinationPath 'C:\ContosoInsurance' -Force
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory('C:\ContosoInsurance.zip','C:\ContosoInsurance')
+
+Copy-Item 'C:\ContosoInsurance\*' -Destination 'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA' -Recurse
+
+function Attach-SqlDatabase {
+    #Add snap-in
+    Add-PSSnapin SqlServerCmdletSnapin* -ErrorAction SilentlyContinue
+
+    $ServerName = 'SQL-SVR'
+    $DatabaseName = 'ContosoInsurance'
+    $FilePath = 'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA\'
+    $MdfFileName = $FilePath + 'ContosoInsurance.mdf'
+    $LdfFileName = $FilePath + 'ContosoInsurance_log.ldf'
+    
+    $AttachCmd = "USE [master] CREATE DATABASE [$DatabaseName] ON (FILENAME ='$MdfFileName'),(FILENAME = '$LdfFileName') for ATTACH"
+    Invoke-Sqlcmd $AttachCmd -QueryTimeout 3600 -ServerInstance $ServerName
+}
+
+Attach-SqlDatabase
