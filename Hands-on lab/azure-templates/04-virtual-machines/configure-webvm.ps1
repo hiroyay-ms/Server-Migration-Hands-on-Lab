f9cf8420b473cd34d06cbddafc74481e89a1d262
Hiroya Yamamoto <hiroyay@microsoft.com>
2020-07-10 23:45:36 +0900

    ファイル コピーの追加

--- Hands-on lab/azure-templates/04-virtual-machines/configure-webvm.ps1
+++ Hands-on lab/azure-templates/04-virtual-machines/configure-webvm.ps1
@@ -17,5 +17,8 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 # Web Server Role
 Install-WindowsFeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Dyn-Compression, Web-Security, Web-Filtering, Web-Basic-Auth, Web-Windows-Auth, Web-App-Dev, Web-Net-Ext45, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Console
 
+# Copy
+Copy-Item 'C:\Contoso' -Destination 'C:\inetpub\wwwroot\' -Recurse
+
 # Time Zone settings
 tzutil /s "Tokyo Standard Time"
