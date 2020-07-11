fb9a3247cb08ac0265555b1eb6061a9eb16cd20e
Hiroya Yamamoto <hiroyay@microsoft.com>
2020-07-11 14:35:44 +0900

    変更

--- Hands-on lab/azure-templates/05-test/configure-webvm.ps1
+++ Hands-on lab/azure-templates/05-test/configure-webvm.ps1
@@ -10,15 +10,5 @@ function Disable-InternetExplorerESC {
 # Disable IE ESC
 Disable-InternetExplorerESC
 
-# Web Server Role
-Install-WindowsFeature Web-Server, Web-WebServer, Web-Common-Http, Web-Default-Doc, Web-Dir-Browsing, Web-Http-Errors, Web-Static-Content, Web-Health, Web-Http-Logging, Web-Performance, Web-Stat-Compression, Web-Dyn-Compression, Web-Security, Web-Filtering, Web-Basic-Auth, Web-Windows-Auth, Web-App-Dev, Web-Net-Ext45, Web-Asp-Net45, Web-ISAPI-Ext, Web-ISAPI-Filter, Web-Mgmt-Console
-
-# Copy
-Invoke-WebRequest 'https://raw.githubusercontent.com/hiroyay-ms/Server-Migration-Hands-on-Lab/master/Hands-on%20lab/src/Contoso.zip' -OutFile 'C:\Contoso.zip'
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory('C:\Contoso.zip','C:\Contoso')
-
-Copy-Item 'C:\Contoso' -Destination 'C:\inetpub\wwwroot\' -Recurse
-
 # Time Zone settings
 tzutil /s "Tokyo Standard Time"
